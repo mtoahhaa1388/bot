f77bb4c50c58a3377a3283f3a8f46c945a580c9a
mtoahhaa1388 <mtoahhaa1388@gmail.com>
2024-03-03 15:50:53 +0330

    gfjhg

--- Send/index.php
+++ Send/index.php
@@ -5,40 +5,40 @@ require_once __DIR__ . "/../Api/index.php";
 require_once __DIR__ . "/../Receive/index.php";
 
 use GuzzleHttp\Client;
-// use Send\Send;
-// $Send = new Send();
+use Send\Send;
+$Send = new Send();
 
 
 
-// $tools = $Tools->ToolsPage();
-// $tool = $Tools->OneTool();
+$tools = $Tools->ToolsPage();
+$tool = $Tools->OneTool();
 
-// $categories = $Category->CategoriesPage();
-// $category = $Category->OneCategory();
-
-
+$categories = $Category->CategoriesPage();
+$category = $Category->OneCategory();
 
 
-// $message = $Receive->text;
 
 
-// if ($message == "/$tools"){
-//     $Send->send($tools); 
-// }
-// elseif ($message == "/$tools/$tool") {
-//     $explode = end(explode("/", $message));
-//     $Send->send($Tools->OneTool($explode));
-// }
-// elseif ($message == "/$categories"){
-//     $Send->send($categories); 
-// }
-// elseif ($message == "/$categories/$category") {
-//     $explode = end(explode("/", $message));
-//     $Send->send($Category->OneCategory($explode));
-// }
+$message = $Receive->text;
 
 
-echo 'xzjhfgizjxhfgui';
+if ($message == "/$tools"){
+    $Send->send($tools); 
+}
+elseif ($message == "/$tools/$tool") {
+    $explode = end(explode("/", $message));
+    $Send->send($Tools->OneTool($explode));
+}
+elseif ($message == "/$categories"){
+    $Send->send($categories); 
+}
+elseif ($message == "/$categories/$category") {
+    $explode = end(explode("/", $message));
+    $Send->send($Category->OneCategory($explode));
+}
+
+
+
 
 
 
