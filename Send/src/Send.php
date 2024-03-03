a8d1e1b179463ab32978a6faf9e5feb6449c7628
mtoahhaa1388 <mtoahhaa1388@gmail.com>
2024-03-03 16:04:20 +0330

    hgdfgrj

--- Send/src/Send.php
+++ Send/src/Send.php
@@ -5,8 +5,29 @@ namespace Send;
 
 class Send {
 
+    public $token;
+    public $inData;
+    public $tData;
+
+    public $message;
+    public $text;
+    public $chat;
+    public $chat_id;
+
+public function __construct() {
+
+     $this->token = "6862940508:AAFdMr40N4CcW8gYH4I3rMD6ka6_6-ADWxg";
+     $this->inData = file_get_contents("php://input");
+     $this->tData = json_decode($this->inData);
+
+     $this->message = $this->tData['message'];
+     $this->text = $this->message['text'];
+     $this->chat = $this->message['chat'];
+     $this->chat_id = $this->chat['id'];
+
+}
     public function bot ($method , $data=[]){
-        $url = "https://ipa.telegram.org/bot" . $token . "/" . $method;
+        $url = "https://ipa.telegram.org/bot" . $this->token . "/" . $method;
         $c = curl_init();
         curl_setopt($c , CURLOPT_URL , $url);
         curl_setopt($c , CURLOPT_RETURNTRANSFER , TRUE);
